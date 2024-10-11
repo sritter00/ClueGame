997c8cf09f443abe0d9edb1198d1e512407ae347
Shane Ritter <sritter@mines.edu>
2024-10-11 12:49:34 -0600

    Set up board.java

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,5 +1,33 @@
 package clueGame;
 
-public class Board {
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import experiment.BoardCell;
+
+public class Board {
+	private BoardCell[][] grid;
+	private Set<BoardCell> targets;
+	private Set<BoardCell> visited;
+	private int numCol = 0;
+	private int numRows = 0;
+	private String layoutConfigFile;
+	private String setupConfigFile;
+	private Map<Character, Room> roomMap;
+	private static Board theInstance = new Board();
+    // constructor is private to ensure only one can be created
+    private Board() {
+           super() ;
+    }
+    // this method returns the only Board
+    public static Board getInstance() {
+           return theInstance;
+    }
+    /*
+     * initialize the board (since we are using singleton pattern)
+     */
+    public void initialize()
+    {
+    }
 }
